ff4c38a9ac3ab31b30d3a951f751d596fb880139
donmigel <elranconarvaes@gmail.com>
2025-02-18 13:48:56 +0100

    readMe file added

--- src/main/java/com/example/movies/config/WebConfig.java
+++ src/main/java/com/example/movies/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(apiKeyInterceptor)
-                .addPathPatterns("/movies/**"); // Apply to specific API paths
+                .addPathPatterns("/movies/**");
     }
 
 
